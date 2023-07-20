0e756775031c57d11f978e36d636a727370a0cd2
VladlenKhan <gitvlad777@gmail.com>
2023-07-20 14:17:34 +0500

    firstupload

---
+++ app.js
@@ -0,0 +1,7 @@
+let tg = window.Telegram.WebApp;
+let buy = document.getElementById('order');
+ 
+document.getElementById('user_name').value = tg.initDataUnsafe.user.first_name + " " + initDataUnsafe.user.last_name
+order.addEventListener('click',()=>{
+    tg.close();
+})
